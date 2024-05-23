31f9207cdec7ee8725fa41014a3738a60227734f
Louis <louis-kaan2002@web.de>
2024-05-23 12:07:49 +0200

    chore: added temporary sign out button

--- src/app/(auth)/(tabs)/notification.tsx
+++ src/app/(auth)/(tabs)/notification.tsx
@@ -5,13 +5,19 @@ import { Button } from 'tamagui';
 
 import useAuth from '../../../hooks/useAuth';
 import { router } from 'expo-router';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 const NotificationPage = () => {
   const { signOut } = useAuth();
   return (
     <SafeAreaView>
       <Text>NotificationPage</Text>
-      <Button onPress={() => signOut()}>Logout</Button>
+      <Button
+        onPress={async () => {
+          signOut();
+        }}>
+        Logout
+      </Button>
       <Button onPress={() => router.navigate('/stream/clw98k2ku00054iqyhs11vhh4')}>
         To stream
       </Button>
