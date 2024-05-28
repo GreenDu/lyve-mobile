4f8fba47a0235e7377899427a248bd8b04eba14c
Louis <louis-kaan2002@web.de>
2024-05-28 11:43:21 +0200

    refactor: added active var to store

--- src/modules/stream/stores/useCurrentStreamInfoStore.ts
+++ src/modules/stream/stores/useCurrentStreamInfoStore.ts
@@ -8,11 +8,13 @@ export const useCurrentStreamInfoStore = create(
       id: '',
       streamer: null as (SocketUser & { followerCount: number }) | null,
       viewerCount: 0,
+      active: false,
     },
     (set) => ({
       setId: (id: string) => set(() => ({ id })),
       setViewerCount: (c: number) => set(() => ({ viewerCount: c })),
       setStreamer: (s: SocketUser & { followerCount: number }) => set(() => ({ streamer: s })),
+      setActive: (a: boolean) => set(() => ({ active: a })),
       reset: () =>
         set(() => ({
           streamId: '',
