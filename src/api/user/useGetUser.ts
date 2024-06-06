b31f436660238f442ada8b8a322e466c82b3bd60
jrau1801 <janrau1802@gmail.com>
2024-06-06 17:30:05 +0200

    chore: added user content to profile

---
+++ src/api/user/useGetUser.ts
@@ -0,0 +1,30 @@
+import type { AxiosError } from 'axios';
+import { createQuery } from 'react-query-kit';
+
+import { TypedResponse } from '../../types/response';
+import { axiosClient } from '../axiosClient';
+
+type Variables = { id: string };
+
+export const useGetUser = createQuery<
+  TypedResponse<{
+    user: {
+      id: string;
+      username: string;
+      dispname: string;
+      avatar_url: string | null;
+      bio: string;
+      followerCount: number;
+      followingCount: number;
+      level: number;
+      promotionPoints: number;
+    };
+  }>,
+  Variables,
+  AxiosError
+>({
+  queryKey: ['users'],
+  fetcher: (variables) => {
+    return axiosClient.get(`api/user/${variables.id}`).then((response) => response.data);
+  },
+});
--- src/screens/ProfilePage.tsx
+++ src/screens/ProfilePage.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from 'react-native';
 import React from 'react';
 import { useGetUser } from '@api/user/useGetUser';
-import { H1, XStack, YStack } from 'tamagui';
+import { H1, H2, SizableText, XStack, YStack } from 'tamagui';
 import UserStats from '@components/profile/ProfileHeader';
 
 const ProfilePage: React.FC<{ userid: string }> = ({ userid }) => {
@@ -12,17 +12,16 @@ const ProfilePage: React.FC<{ userid: string }> = ({ userid }) => {
       <YStack height="100%" backgroundColor="$color.background">
         <YStack height="35%" backgroundColor={'$color.accentDark'} borderRadius={30}>
           <YStack top="$5" left="$5">
-          <UserStats 
-          followerCount={data?.data.user.followerCount}
-          followingCount={data?.data.user.followingCount}
-          ></UserStats>
+            <UserStats
+              followerCount={data?.data.user.followerCount}
+              followingCount={data?.data.user.followingCount}></UserStats>
 
-          <XStack>
-            <Text>hallo</Text>
-          </XStack>
+            <YStack justifyContent="flex-start">
+              <H2>{data?.data.user.username}</H2>
+              <SizableText>@{data?.data.user.username}</SizableText>
+              <SizableText>{data?.data.user.bio}This is a user bio</SizableText>
+            </YStack>
           </YStack>
-
-
         </YStack>
       </YStack>
     </View>
