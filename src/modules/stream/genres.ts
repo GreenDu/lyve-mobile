8302f5406dd81783206ddd3fb68b046e3cac4c37
Louis <louis-kaan2002@web.de>
2024-06-06 21:35:08 +0200

    refactor: refactored create stream page

---
+++ src/modules/stream/genres.ts
@@ -0,0 +1,67 @@
+export const genres = [
+  {
+    text: 'Chatting 💬',
+    color: '#E16F6F',
+    selected: false,
+  },
+  {
+    text: 'Beauty 💄',
+    color: '#E16FDD',
+    selected: false,
+  },
+  {
+    text: 'Gaming 🎮',
+    color: '#6FB1E1',
+    selected: false,
+  },
+  {
+    text: 'Nature 🌳',
+    color: '#85D95D',
+    selected: false,
+  },
+  {
+    text: 'Sport 🏈',
+    color: '#FBC151',
+    selected: false,
+  },
+  {
+    text: 'Art 🎨',
+    color: '#E4D3A6',
+    selected: false,
+  },
+  {
+    text: 'IRL 😄',
+    color: '#F4F2C3',
+    selected: false,
+  },
+  {
+    text: 'Football ⚽️',
+    color: '#DDDDDD',
+    selected: false,
+  },
+  {
+    text: 'Cooking 🍜',
+    color: '#FFE9CE',
+    selected: false,
+  },
+  {
+    text: 'Music 🎶',
+    color: '#D283F8',
+    selected: false,
+  },
+  {
+    text: 'Podcast 🎙️',
+    color: '#C2C2C2',
+    selected: false,
+  },
+  {
+    text: 'Education 👨‍🏫',
+    color: '#E6E890',
+    selected: false,
+  },
+  {
+    text: 'Camping  ⛺️',
+    color: '#2AE3A1',
+    selected: false,
+  },
+];
