255f461e95d36f37ff1068b33d9045660a33261b
jrau1801 <janrau1802@gmail.com>
2024-06-06 15:28:48 +0200

    style: changed preview images

--- src/screens/HomePage.tsx
+++ src/screens/HomePage.tsx
@@ -14,27 +14,27 @@ const HomePage = () => {
     {
       viewerCount: 1200000,
       streamerName: 'Nic',
-      previewImg: 'https://random.imagecdn.app/181/310',
+      previewImg: 'https://random.imagecdn.app/182/310',
     },
     {
       viewerCount: 10000,
       streamerName: 'Tom',
-      previewImg: 'https://random.imagecdn.app/181/310',
+      previewImg: 'https://random.imagecdn.app/183/310',
     },
     {
       viewerCount: 100000,
       streamerName: 'Tom',
-      previewImg: 'https://random.imagecdn.app/181/310',
+      previewImg: 'https://random.imagecdn.app/184/310',
     },
     {
       viewerCount: 1200,
       streamerName: 'Julia',
-      previewImg: 'https://random.imagecdn.app/181/310',
+      previewImg: 'https://random.imagecdn.app/185/310',
     },
     {
       viewerCount: 80,
       streamerName: 'Kevin',
-      previewImg: 'https://random.imagecdn.app/181/310',
+      previewImg: 'https://random.imagecdn.app/186/310',
     },
   ];
   return (
