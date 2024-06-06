0bc194329241f3b90ad2472672bf6381224f166b
jrau1801 <janrau1802@gmail.com>
2024-06-06 23:33:18 +0200

    feat: added account tab

--- src/screens/SettingsPage.tsx
+++ src/screens/SettingsPage.tsx
@@ -1,6 +1,6 @@
 import { View, Text } from 'react-native';
 import React from 'react';
-import { YStack, XStack, H1, Button } from 'tamagui';
+import { YStack, XStack, H1, Button, H2 } from 'tamagui';
 import { Feather } from '@expo/vector-icons';
 
 const SettingsPage: React.FC<{ userid: string }> = ({ userid }) => {
@@ -8,17 +8,17 @@ const SettingsPage: React.FC<{ userid: string }> = ({ userid }) => {
     <View>
       <YStack height="100%" padding="$4">
         <XStack alignItems="center">
-          <Button 
-          icon={<Feather name="chevron-left" size={24} color="white" />} 
-          backgroundColor="black">
-            
-          </Button>
-          <H1 fontSize={32}>
-            Settings
-            </H1>
+          <Button
+            icon={<Feather name="chevron-left" size={24} color="white" />}
+            backgroundColor="black"></Button>
+          <H1 fontSize={32}>Settings</H1>
         </XStack>
+
+        <H2 mt="$10" fontSize={24} mb="$3">
+          Account
+        </H2>
+        <YStack height="20%" borderRadius={30} backgroundColor="green"></YStack>
       </YStack>
-      <Text>SettingsPage</Text>
     </View>
   );
 };
