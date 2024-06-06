c288de7f973c7287841284d33fc33c700fab87fe
jrau1801 <janrau1802@gmail.com>
2024-06-06 16:01:54 +0200

    chore: started profile modal

--- src/screens/ProfilePage.tsx
+++ src/screens/ProfilePage.tsx
@@ -10,15 +10,17 @@ const { data } = useGetUser({ variables: { id: userid }});
 
   return (
     <View>
-      <Text style = {{color: "white"}}>ProfilePage {JSON.stringify(data)} </Text>
-      <YStack height="100%" backgroundColor="$color.background" padding="$4">
-      <Avatar circular size="$5">
+      <YStack height="100%" backgroundColor="$color.background">
+        <YStack height="35%" backgroundColor={'$color.accentDark'} borderRadius={30}>
+        <Avatar circular size="$8">
           <Avatar.Image
             accessibilityLabel="Nate Wienert"
             src="https://images.unsplash.com/photo-1531384441138-2736e62e0919?&w=100&h=100&dpr=2&q=80"
           />
           <Avatar.Fallback delayMs={600} backgroundColor="$blue10" />
         </Avatar>
+        </YStack>
+      
         </YStack>
     </View>
   )
