3204e5832072a3139ef81a4397f4e82815cf835f
jrau1801 <janrau1802@gmail.com>
2024-06-11 02:59:15 +0200

    style: icon background color changed

--- src/components/profile/GenreBadge.tsx
+++ src/components/profile/GenreBadge.tsx
@@ -9,7 +9,7 @@ interface Props {
 const ProfileHeader: React.FC<Props> = ({ genre = "NaN", percent = 0 }) => {
   return (
     <YStack alignItems='center' backgroundColor="$primaryLight"  height="$12" width="$10" borderRadius={30}>
-        <YStack mt="$3" height="$5" width="$5" backgroundColor="green" borderRadius={90}></YStack>
+        <YStack mt="$3" height="$5" width="$5" backgroundColor="#151718" borderRadius={90}></YStack>
         <SizableText>
             fhrb
         </SizableText>
