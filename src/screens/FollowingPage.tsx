b6b240e283d71feaf08525b77cf66fe3f7f5159c
jrau1801 <janrau1802@gmail.com>
2024-06-12 01:01:57 +0200

    feat: first implementation of followingpage

--- src/screens/FollowingPage.tsx
+++ src/screens/FollowingPage.tsx
@@ -1,12 +1,42 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, ScrollView } from 'react-native';
+import React, { useEffect } from 'react';
+import { useGetFollowedBy } from '@api/user/query/useGetFollowedBy';
+import { H1, SizableText, YStack } from 'tamagui';
+import UserFollowerCard from '@components/UserFollowerCard';
 
-const FollowingPage = () => {
+const FollowingPage: React.FC<{ id: string }> = ({ id }) => {
+  console.log(id);
+  const { data, isSuccess } = useGetFollowedBy({ variables: { id } });
+
+  useEffect(() => {
+    console.log(data);
+  }, [data]);
+
   return (
-    <View>
-      <Text>FollowingPage</Text>
-    </View>
-  )
-}
+    <YStack padding="$4" height="100%">
+      <H1 marginVertical="$4" fontSize={24} fontWeight="800">
+        Followers
+      </H1>
+      {isSuccess && (
+        <ScrollView>
+          <YStack>
+            {data?.data?.user.followedBy.map((u) => {
+              return (
+                <UserFollowerCard
+                  key={u.user.id}
+                  id={u.user.id}
+                  username={u.user.username}
+                  dispname={u.user.dispname}
+                  avatar_url={u.user.avatar_url}
+                  subscribed={u.subscribed}
+                />
+              );
+            })}
+          </YStack>
+        </ScrollView>
+      )}
+    </YStack>
+  );
+};
 
 export default FollowingPage;
