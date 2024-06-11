c57fb891a3730225fd1f84b2db53942d5a772547
jrau1801 <janrau1802@gmail.com>
2024-06-11 02:50:07 +0200

    feat: content template for genre badge

--- src/components/profile/GenreBadge.tsx
+++ src/components/profile/GenreBadge.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { XStack, YStack, H2, Avatar, Separator } from 'tamagui';
+import { XStack, YStack, H2, Avatar, Separator, SizableText } from 'tamagui';
 
 interface Props {
   genre?: string | undefined;
@@ -8,7 +8,18 @@ interface Props {
 
 const ProfileHeader: React.FC<Props> = ({ genre = "NaN", percent = 0 }) => {
   return (
-    <YStack backgroundColor="$primaryLight"  height="$12" width="$10" borderRadius={30}></YStack>
+    <YStack alignItems='center' backgroundColor="$primaryLight"  height="$12" width="$10" borderRadius={30}>
+        <YStack mt="$3" height="$5" width="$5" backgroundColor="green" borderRadius={90}></YStack>
+        <SizableText>
+            fhrb
+        </SizableText>
+
+        <SizableText>
+            fhrb
+        </SizableText>
+
+
+    </YStack>
   );
 };
 
