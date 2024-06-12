47538cda6ab36b972c99de0e345e753cfead58db
jrau1801 <janrau1802@gmail.com>
2024-06-12 19:27:44 +0200

    feat: back button on following page

--- src/screens/FollowingPage.tsx
+++ src/screens/FollowingPage.tsx
@@ -1,8 +1,10 @@
 import { View, Text, ScrollView } from 'react-native';
 import React, { useEffect } from 'react';
 import { useGetFollowing } from '@api/user/query/useGetFollowing';
-import { H1, SizableText, YStack } from 'tamagui';
+import { Button, H1, SizableText, XStack, YStack } from 'tamagui';
 import UserFollowerCard from '@components/UserFollowerCard';
+import { Feather } from '@expo/vector-icons';
+import { router } from 'expo-router';
 
 const FollowingPage: React.FC<{ id: string }> = ({ id }) => {
   console.log(id);
@@ -14,9 +16,16 @@ const FollowingPage: React.FC<{ id: string }> = ({ id }) => {
 
   return (
     <YStack padding="$4" height="100%">
+      <XStack alignItems='center'>
+      <Button
+        backgroundColor="#151718"
+        icon={<Feather name="chevron-left" size={24} color="white" />}
+        onPress={() => router.back()}
+      />
       <H1 marginVertical="$4" fontSize={24} fontWeight="800">
         Following
       </H1>
+      </XStack>
       {isSuccess && (
         <ScrollView>
           <YStack>
