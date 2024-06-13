c4196c703e00b955d1608a49e33005f0bddf108c
jrau1801 <janrau1802@gmail.com>
2024-06-13 17:23:44 +0200

    fix: smaller buttons on profile page

--- src/screens/ProfilePage.tsx
+++ src/screens/ProfilePage.tsx
@@ -43,13 +43,13 @@ const ProfilePage: React.FC<{ userid: string }> = ({ userid }) => {
           <XStack justifyContent="center" mt="$5">
             <Button
               flex={1}
-              size="$5"
+              size="$4"
               maxWidth="40%"
               borderRadius={20}
               backgroundColor="#A372F9">
               Statistics
             </Button>
-            <Button flex={1} size="$5" maxWidth="40%" borderRadius={20}>
+            <Button flex={1} size="$4" maxWidth="40%" borderRadius={20}>
               Achievements
             </Button>
           </XStack>
