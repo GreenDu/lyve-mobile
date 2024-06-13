ad61395207aafad9d86779553f5711a03cdf613f
Louis <louis-kaan2002@web.de>
2024-06-13 19:50:40 +0200

    feat: added SwitchButton

--- src/screens/ProfilePage.tsx
+++ src/screens/ProfilePage.tsx
@@ -1,17 +1,27 @@
-import { User, Achievement, Stream } from '@api/responses';
+import { User } from '@api/responses';
 import { useGetUser } from '@api/user/query/useGetUser';
-import ProfileHeader from '@components/profile/ProfileHeader';
+import SwitchButton from '@components/SwitchButton';
 import GenreBadge from '@components/profile/GenreBadge';
-import { Feather } from '@expo/vector-icons';
-import { router } from 'expo-router';
+import ProfileHeader from '@components/profile/ProfileHeader';
 import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, ScrollView, View } from 'react-native';
-import { Button, H3, SizableText, XStack, YStack } from 'tamagui';
+import { ActivityIndicator, ScrollView } from 'react-native';
+import { XStack, YStack } from 'tamagui';
 
+type States = 'Statistics' | 'Achievements';
 const ProfilePage: React.FC<{ userid: string }> = ({ userid }) => {
-  const { data, isFetching } = useGetUser({ variables: { id: userid }, refetchOnMount: true, refetchOnWindowFocus: true });
+  const { data, isFetching } = useGetUser({
+    variables: { id: userid },
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
+  });
 
   const [userData, setUserData] = useState<User>();
+
+  const [activeState, setActiveState] = useState<States>('Statistics');
+
+  const handleStateChange = (newState: string) => {
+    setActiveState(newState as States);
+  };
 
   useEffect(() => {
     if (data && data.data) {
@@ -33,42 +43,35 @@ const ProfilePage: React.FC<{ userid: string }> = ({ userid }) => {
   }
 
   return (
-    <ScrollView>
-      <YStack height="100%" backgroundColor="$color.background">
-        <ProfileHeader user={userData!} />
+    <YStack height="100%" backgroundColor="$color.background">
+      <ProfileHeader user={userData!} />
 
-        <YStack flex={1}>
-          {/* Button component for statistics and achievements*/}
+      <YStack flex={1}>
+        {/* Button component for statistics and achievements*/}
 
-          <XStack justifyContent="center" mt="$5">
-            <Button
-              flex={1}
-              size="$4"
-              maxWidth="40%"
-              borderRadius={20}
-              backgroundColor="#A372F9">
-              Statistics
-            </Button>
-            <Button flex={1} size="$4" maxWidth="40%" borderRadius={20}>
-              Achievements
-            </Button>
-          </XStack>
+        <XStack justifyContent="center" paddingVertical="$4">
+          <SwitchButton
+            states={['Statistics', 'Achievements'] as [States, States]}
+            onStateChange={handleStateChange}
+          />
+        </XStack>
+        <ScrollView bounces>
           <YStack gap="$-8" marginBottom="$8">
-          <XStack flex={1} justifyContent="center" gap="$6" padding="$7">
-            <GenreBadge genre='Education 👨‍🏫' percent={30}/>
-            <GenreBadge genre='Music 🎶' percent={20}/>
-            <GenreBadge genre='Football ⚽️'percent={13}/>
-          </XStack>
+            <XStack flex={1} justifyContent="center" gap="$6" padding="$7">
+              <GenreBadge genre="Education 👨‍🏫" percent={30} />
+              <GenreBadge genre="Music 🎶" percent={20} />
+              <GenreBadge genre="Football ⚽️" percent={13} />
+            </XStack>
 
-          <XStack flex={1} justifyContent="center" gap="$6" padding="$7">
-            <GenreBadge genre='IRL 😄' percent={9}/>
-            <GenreBadge genre='Art 🎨' percent={5}/>
-            <GenreBadge genre='Sport 🏈' percent={2}/>
-          </XStack>
+            <XStack flex={1} justifyContent="center" gap="$6" padding="$7">
+              <GenreBadge genre="IRL 😄" percent={9} />
+              <GenreBadge genre="Art 🎨" percent={5} />
+              <GenreBadge genre="Sport 🏈" percent={2} />
+            </XStack>
           </YStack>
-        </YStack>
+        </ScrollView>
       </YStack>
-    </ScrollView>
+    </YStack>
   );
 };
 
