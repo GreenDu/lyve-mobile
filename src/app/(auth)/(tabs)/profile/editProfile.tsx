3e7f7888bb3407923ee1c184d6243ba8e6fe2285
jrau1801 <janrau1802@gmail.com>
2024-06-14 20:13:31 +0200

    style: edit profile is now safeareaview

--- src/app/(auth)/(tabs)/profile/editProfile.tsx
+++ src/app/(auth)/(tabs)/profile/editProfile.tsx
@@ -1,12 +1,13 @@
 import { View, Text } from 'react-native'
 import React from 'react'
 import EditProfilePage from '@screens/EditProfilePage'
+import { SafeAreaView } from 'react-native-safe-area-context'
 
 const EditProfile = () => {
   return (
-    <View>
+    <SafeAreaView>
       <EditProfilePage></EditProfilePage>
-    </View>
+    </SafeAreaView>
   )
 }
 
