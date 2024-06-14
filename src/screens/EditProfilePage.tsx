043ddc48760f3f10d2beb25e2d15a3430ce11b3e
jrau1801 <janrau1802@gmail.com>
2024-06-14 20:14:02 +0200

    feat: avatar display on edit profile page

--- src/screens/EditProfilePage.tsx
+++ src/screens/EditProfilePage.tsx
@@ -1,27 +1,51 @@
-import { View, Text, ActivityIndicator, ScrollView } from 'react-native';
 import React from 'react';
 import useAuth from '@modules/auth/useAuth';
-import { useGetNotifications } from '@api/user/query/useGetNotifications';
-import { H1, SizableText, YStack } from 'tamagui';
-import { GetNotificationsResponse, Notification } from '@api/responses';
-import NotificationCard from '@components/notification/NotificationCard';
+import {
+  Avatar,
+  Button,
+  H1,
+  SizableText,
+  XStack,
+  YStack,
+} from 'tamagui';
+import { Feather } from '@expo/vector-icons';
+import { router } from 'expo-router';
+
 
 const EditProfilePage = () => {
   const { user } = useAuth();
 
-
-
   return (
-    <YStack padding="$4" height="100%">
-      <H1 marginVertical="$4" fontSize={24} fontWeight="800">
-        Edit Profile
-      </H1>
-
-      <ScrollView bounces>
-        <YStack space="$3">
-
-        </YStack>
-      </ScrollView>
+    <YStack padding="$4" backgroundColor="$background" gap="$5">
+      <XStack alignItems="center" justifyContent="space-between">
+        <XStack alignItems="center">
+          <Button
+            backgroundColor="#151718"
+            icon={<Feather name="chevron-left" size={24} color="white" />}
+            onPress={() => router.back()}
+          />
+          <H1 fontSize={24}>Edit Profile</H1>
+        </XStack>
+        <SizableText fontSize={18} color="$accentWashedOut">
+          Finish
+        </SizableText>
+      </XStack>
+      <YStack alignItems="center">
+        <Avatar circular size="$10">
+          <Avatar.Image
+            accessibilityLabel="Cam"
+            src="https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=80"
+          />
+          <Avatar.Fallback backgroundColor="$blue10" />
+        </Avatar>
+        <SizableText color="$textWashedOut">Edit Avatar</SizableText>
+      </YStack>
+      <YStack gap="$2">
+        <XStack justifyContent="space-between">
+          <SizableText>Display-name</SizableText>
+          <SizableText>0/20</SizableText>
+        </XStack>
+      </YStack>
     </YStack>
   );
 };
