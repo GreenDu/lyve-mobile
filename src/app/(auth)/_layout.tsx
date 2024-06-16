4c8978d8ea855d8b9a12f8b23843824ebf850fe4
Louis <louis-kaan2002@web.de>
2024-06-16 21:52:27 +0200

    feat: added prefetching

--- src/app/(auth)/_layout.tsx
+++ src/app/(auth)/_layout.tsx
@@ -1,3 +1,6 @@
+import { queryClient } from '@api/APIProvider';
+import { useGetNotifications } from '@api/user/query/useGetNotifications';
+import { useGetUser } from '@api/user/query/useGetUser';
 import useAuth from '@modules/auth/useAuth';
 import { Redirect, Stack } from 'expo-router';
 import React from 'react';
@@ -5,7 +8,7 @@ import { ActivityIndicator } from 'react-native';
 import { YStack } from 'tamagui';
 
 export default function AppLayout() {
-  const { isLoading, session } = useAuth();
+  const { isLoading, session, user } = useAuth();
 
   if (isLoading) {
     return (
@@ -19,6 +22,11 @@ export default function AppLayout() {
 
   if (!session) {
     return <Redirect href="/login" />;
+  }
+
+  if (user) {
+    queryClient.prefetchQuery(useGetUser.getFetchOptions({ id: user.id }));
+    queryClient.prefetchQuery(useGetNotifications.getFetchOptions({ id: user.id }));
   }
 
   return (
