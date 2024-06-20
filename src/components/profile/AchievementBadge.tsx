51d2c6c17cee669abc52e06d8749e96ab373bbbd
jrau1801 <janrau1802@gmail.com>
2024-06-20 22:43:15 +0200

    feat: description to achievements

--- src/components/profile/AchievementBadge.tsx
+++ src/components/profile/AchievementBadge.tsx
@@ -1,9 +1,10 @@
 import { AchievementType } from '@api/responses';
 import { LevelType } from '../../types/types';
-import React from 'react';
-import { YStack, SizableText, XStack, Progress } from 'tamagui';
+import React, { useState } from 'react';
+import { YStack, SizableText, XStack, Progress, Accordion } from 'tamagui';
 import { achievementLookupTable } from '@utils/achievementLookup';
-
+import { Pressable } from 'react-native';
+import { Feather } from '@expo/vector-icons';
 
 interface Props {
   name: string;
@@ -14,26 +15,51 @@ interface Props {
 }
 
 const AchievementBadge: React.FC<Props> = ({ name, condition, progress, type, level }) => {
+  const [showDescription, setShowDescription] = useState(false);
+
+  const toggleDescription = () => {
+    setShowDescription(!showDescription);
+  };
 
   return (
-    <YStack padding="$2" backgroundColor="$primaryLight" borderRadius={15}>
-      <XStack gap="$2">
-
-        <YStack borderRadius={15} backgroundColor={"black"} height="$4" width="$4" alignItems='center' justifyContent='center'>
+    <Pressable onPress={toggleDescription}>
+      <YStack padding="$2" backgroundColor="$primaryLight" borderRadius={15}>
+        <XStack gap="$2">
+          <YStack
+            borderRadius={15}
+            backgroundColor={'black'}
+            height="$4"
+            width="$4"
+            alignItems="center"
+            justifyContent="center">
             <SizableText>{achievementLookupTable[type][level as LevelType]}</SizableText>
-        </YStack>
-        <YStack flex={1} gap="$2" paddingRight="$2">
-        <XStack justifyContent='space-between'>
-            <SizableText>{name}</SizableText>
-            <SizableText>{progress} / {condition}</SizableText>
-        </XStack>
+          </YStack>
+          <YStack flex={1} gap="$2" paddingRight="$2">
+            <XStack justifyContent="space-between">
+              <SizableText>{name}</SizableText>
+              <SizableText>
+                {progress} / {condition}
+              </SizableText>
+            </XStack>
 
-        <Progress value={Math.round((progress/condition) * 100)}>
-        <Progress.Indicator backgroundColor="$textWashedOut" />
-        </Progress>
-        </YStack>
-      </XStack>
-    </YStack>
+            <Progress value={Math.round((progress / condition) * 100)}>
+              <Progress.Indicator backgroundColor="$textWashedOut" />
+            </Progress>
+          </YStack>
+        </XStack>
+      </YStack>
+
+      {showDescription && (
+        <XStack alignItems='center'>
+          <Feather name="corner-down-right" size={24} color="white" />
+          <YStack flex={1} padding="$2" backgroundColor="$primaryLight" borderRadius={15}>
+            <SizableText>
+              
+            </SizableText>
+          </YStack>
+        </XStack>
+      )}
+    </Pressable>
   );
 };
 
