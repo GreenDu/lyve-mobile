fc796f013e76bc4ec950fa449b29a9e529cef6c2
jrau1801 <janrau1802@gmail.com>
2024-06-20 18:05:29 +0200

    feat: KeyBoardAvoidingView on edit Profile

--- src/app/(auth)/(tabs)/profile/editProfile.tsx
+++ src/app/(auth)/(tabs)/profile/editProfile.tsx
@@ -1,14 +1,30 @@
-import { View, Text } from 'react-native'
-import React from 'react'
-import EditProfilePage from '@screens/EditProfilePage'
-import { SafeAreaView } from 'react-native-safe-area-context'
+import {
+  View,
+  Text,
+  KeyboardAvoidingView,
+  Platform,
+  ScrollView,
+  TouchableWithoutFeedback,
+  Keyboard,
+} from 'react-native';
+import React from 'react';
+import EditProfilePage from '@screens/EditProfilePage';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const EditProfile = () => {
   return (
-    <SafeAreaView>
-      <EditProfilePage></EditProfilePage>
-    </SafeAreaView>
-  )
-}
+    <KeyboardAvoidingView
+      behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
+      style={{ flex: 1 }}>
+      <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
+        <SafeAreaView style={{ flex: 1 }}>
+          <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
+            <EditProfilePage />
+          </ScrollView>
+        </SafeAreaView>
+      </TouchableWithoutFeedback>
+    </KeyboardAvoidingView>
+  );
+};
 
-export default EditProfile
+export default EditProfile;
