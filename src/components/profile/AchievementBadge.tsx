817ae1b517c631c77615c36cb67275ae663a405a
jrau1801 <janrau1802@gmail.com>
2024-06-20 21:39:43 +0200

    chore: lookup table and level to parameter

--- src/components/profile/AchievementBadge.tsx
+++ src/components/profile/AchievementBadge.tsx
@@ -1,14 +1,45 @@
 import React from 'react';
 import { YStack, SizableText, XStack, Progress } from 'tamagui';
 
+const achievementEmojis = {
+    'NTH_STREAM': {
+      1: '1️⃣',
+      2: '🔟',
+      3: '🔥',
+      4: '🎥',
+      5: '💯',
+      6: '🏆'
+    },
+    'NTH_VIEWER': {
+      1: '🔟',
+      2: '📈',
+      3: '💯',
+      4: '✨',
+      5: '💛',
+      6: '🌟',
+      7: '👑',
+      8: '🌏'
+    },
+    'MINUTES_STREAMED': {
+      1: '🔟',
+      2: '⌛️',
+      3: '🏃‍➡️',
+      4: '🔥',
+      5: '🌄',
+      6: '🤯',
+      7: '❤️‍🔥'
+    }
+  };
+
 interface Props {
   name: string;
   condition: number;
   progress: number;
   type: string;
+  level: number;
 }
 
-const AchievementBadge: React.FC<Props> = ({ name, condition, progress, type }) => {
+const AchievementBadge: React.FC<Props> = ({ name, condition, progress, type, level }) => {
   return (
     <YStack padding="$2" backgroundColor="$primaryLight" borderRadius={15}>
       <XStack gap="$2">
