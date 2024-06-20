2ff884b8b669cb035881c6cbfdc2f06d347c18ba
jrau1801 <janrau1802@gmail.com>
2024-06-20 16:32:55 +0200

    feat: update bio and displayname with Formik onPress()

--- src/screens/EditProfilePage.tsx
+++ src/screens/EditProfilePage.tsx
@@ -5,80 +5,111 @@ import { Feather } from '@expo/vector-icons';
 import { router } from 'expo-router';
 import { Formik } from 'formik';
 import { Pressable, TextInput, View } from 'react-native';
+import { useUpdateUser } from '@api/user/mutation/useUpdateUser';
+import { useGetUser } from '@api/user/query/useGetUser';
 
 const EditProfilePage = () => {
   const { user } = useAuth();
 
+  const { data } = useGetUser({
+    variables: { id: user.id },
+    refetchOnMount: true,
+    refetchOnWindowFocus: true,
+  });
+
+  const { mutate } = useUpdateUser();
+
+  const updateUser = async (displayname: string, bio: string) => {
+    const formData = new FormData();
+    formData.append('dispname', displayname);
+    formData.append('bio', bio);
+    mutate({
+      id: user.id,
+      data: formData,
+    });
+  };
+
+  const handleFinishPress = (displayname: string, bio: string) => {
+    updateUser(displayname, bio);
+    router.back();
+  };
+
   return (
-    <YStack padding="$4" backgroundColor="$background" gap="$5" height="100%">
-      <XStack alignItems="center" justifyContent="space-between">
-        <XStack alignItems="center">
-          <Pressable onPress={() => router.back()}>
-            <Feather name="chevron-left" size={28} color="white" />
-          </Pressable>
-          <H1 fontSize={24}>Edit Profile</H1>
-        </XStack>
-        <SizableText fontSize={18} color="$accentWashedOut">
-          Finish
-        </SizableText>
-      </XStack>
+    <Formik
+      initialValues={{ displayname: user.dispname || '', bio: data?.data?.user.bio || '' }}
+      onSubmit={(values) => updateUser(values.displayname, values.bio)}
+    >
+      {(props) => (
+          <YStack padding="$4" backgroundColor="$background" gap="$5" height="100%">
+            <XStack alignItems="center" justifyContent="space-between">
+              <XStack alignItems="center">
+                <Pressable onPress={() => router.back()}>
+                  <Feather name="chevron-left" size={28} color="white" />
+                </Pressable>
+                <H1 fontSize={24}>Edit Profile</H1>
+              </XStack>
+              <Pressable onPress={() => handleFinishPress(props.values.displayname, props.values.bio)}>
+                <SizableText fontSize={18} color="$accentWashedOut">
+                  Finish
+                </SizableText>
+              </Pressable>
+            </XStack>
 
-      <YStack alignItems="center">
-        <Avatar circular size="$10">
-          <Avatar.Image
-            accessibilityLabel="Cam"
-            src="https://images.unsplash.com/photo-1548142813-c348350df52b?&w=150&h=150&dpr=2&q=80"
-          />
-          <Avatar.Fallback backgroundColor="$blue10" />
-        </Avatar>
-        <SizableText color="$textWashedOut">Edit Avatar</SizableText>
-      </YStack>
-
-      <YStack gap="$2">
-        <Formik initialValues={{ displayname: '', bio: '' }} onSubmit={(values) => {}}>
-          {(props) => (
-            <YStack gap="$6">
-              <YStack gap="$2">
-                <XStack justifyContent="space-between">
-                  <SizableText color="$textWashedOut">Displayname</SizableText>
-                  <SizableText
-                    color={props.values.displayname.length >= 20 ? 'red' : '$textWashedOut'}>
-                    {props.values.displayname.length}/20
-                  </SizableText>
-                </XStack>
-                <Input
-                  maxLength={20}
-                  placeholderTextColor="$textWashedOut"
-                  placeholder="displayname"
-                  onChangeText={(text) => {
-                    props.handleChange('displayname')(text);
-                  }}
-                  value={props.values.displayname}
+            <YStack alignItems="center">
+              <Avatar circular size="$10" testID="profile-avatar">
+                <Avatar.Image
+                  accessibilityLabel="Nate Wienert"
+                  src={
+                    user?.avatar_url !== ''
+                      ? user.avatar_url
+                      : 'https://lyveblobstorage.blob.core.windows.net/images/avatar_placeholder.png'
+                  }
                 />
-              </YStack>
+                <Avatar.Fallback delayMs={600} backgroundColor="$blue10" />
+              </Avatar>
+              <SizableText color="$textWashedOut">Edit Avatar</SizableText>
+            </YStack>
 
-              <YStack gap="$2">
-                <XStack justifyContent="space-between">
-                  <SizableText color="$textWashedOut">Bio</SizableText>
-                  <SizableText color={props.values.bio.length >= 100 ? 'red' : '$textWashedOut'}>
-                    {props.values.bio.length}/100
-                  </SizableText>
-                </XStack>
-                <TextArea
-                  maxLength={100}
-                  placeholderTextColor="$textWashedOut"
-                  placeholder="bio"
-                  onChangeText={(text) => {
-                    props.handleChange('bio')(text);
-                  }}
-                  value={props.values.bio}
-                />
+            <YStack gap="$2">
+              <YStack gap="$6">
+                <YStack gap="$2">
+                  <XStack justifyContent="space-between">
+                    <SizableText color="$textWashedOut">Displayname</SizableText>
+                    <SizableText
+                      color={props.values.displayname.length >= 20 ? 'red' : '$textWashedOut'}
+                    >
+                      {props.values.displayname.length}/20
+                    </SizableText>
+                  </XStack>
+                  <Input
+                    maxLength={20}
+                    placeholderTextColor="$textWashedOut"
+                    placeholder="displayname"
+                    onChangeText={props.handleChange('displayname')}
+                    value={props.values.displayname}
+                  />
+                </YStack>
+
+                <YStack gap="$2">
+                  <XStack justifyContent="space-between">
+                    <SizableText color="$textWashedOut">Bio</SizableText>
+                    <SizableText color={props.values.bio.length >= 100 ? 'red' : '$textWashedOut'}>
+                      {props.values.bio.length}/100
+                    </SizableText>
+                  </XStack>
+                  <TextArea
+                    maxLength={100}
+                    placeholderTextColor="$textWashedOut"
+                    placeholder="bio"
+                    onChangeText={props.handleChange('bio')}
+                    value={props.values.bio}
+                  />
+                </YStack>
               </YStack>
             </YStack>
-          )}
-        </Formik>
-      </YStack>
-    </YStack>
+          </YStack>
+      )}
+    </Formik>
   );
 };
 
